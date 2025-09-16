a7f06ff362440f1d1a8817ae1546b998de3d4d70
Dcarp5610 <dannycol95@gmail.com>
2025-09-16 10:04:39 -0600

    second commit

---
+++ MyDemo1.py
@@ -0,0 +1,22 @@
+x = 8
+y = 9.9
+
+morning_talk = "Hello, what is the weather today?"
+response = " degrees, a little chilly"
+
+cold = True
+warm = "degrees"
+afternoon = "Will it get warmer?"
+response_2 = "It will get to"
+v = 2
+z = v==2
+
+print(morning_talk)
+print(x,response)
+print(cold)
+print(afternoon)
+print(response_2,y,warm)
+print(z+1)
+
+z = x=3
+print(z+1)
