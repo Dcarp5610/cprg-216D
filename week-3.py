004f45c7e5869d6a0dc612501a64f18bd49d9776
Dcarp5610 <dannycol95@gmail.com>
2025-09-19 15:58:30 -0600

    what statments do

---
+++ week-3.py
@@ -0,0 +1,60 @@
+
+# what statements do we know so far
+
+# assignment statement
+i= 3 # Asking to reserve memory of 4 bytes (this is the size of an integer)
+        # an put the value 3 in it
+f= 3.4
+msg = "Hello"
+message = "Hola"
+condition = True
+# a variable is a mermory that has a size 
+# a name, an address, a value, and a type
+
+# Another statement is a function call
+# The usage of the function is called a function call
+
+print("Hello World!") # The function takes an argunment and prints the value of it on screen
+
+print(3)
+print(f)
+print(condition)
+
+print(i, type(i))
+print(f, type(f))
+print(msg, type(msg))
+print(condition, type(condition))
+
+# a function call has a name, and () and in between we have an input
+
+
+
+y = 3+4 # this evaluates to 7 so i evaluate the exp and put it in y
+
+username = input("What is your name?") # this will evaluate to a variable
+
+print("Hello",username)
+
+# the casting functions
+#implicit casting
+
+x = 3
+y = 4
+z = x//y # floor division, rounds up to the smaller number
+num = 14.5
+age = int(num) # a castin function
+print("Value of z is", z, ". Type of z is", type(z))
+print(age)
+y = 4
+yf = float(y)
+print(yf)
+text = str(y)
+print(text)
+
+text_as_num = int(text)
+print(text_as_num + 7)
+
+
+# a function does something.
+# but a function can also take input and output
+
